68302ed92f21d6dae52a009fae27bccc951d9e12
unknown <wederlinodesouza@gmail.com>
2023-01-04 18:51:06 -0300

    Criando o terceiro componente com exportação múltipla

--- desenv/src/index.js
+++ desenv/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Primeiro from './Primeiro';
 import BomDia from './BomDia';
+import { BoaNoite, BoaTarde } from './Multiplos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BomDia nome= "Weder" idade={21} ></BomDia>)
+root.render(
+  <div>
+    <BoaTarde nome= "Weder">, </BoaTarde>
+    <BoaNoite nome= "Mario">, </BoaNoite>
+  </div>
+)
